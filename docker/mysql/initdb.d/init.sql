03c46f34ac3832a14fd7c111e277a3a70def1e75
欸必 <2955671840@qq.com>
2025-09-01 11:00:45 +0800

    改用docker-compose形式的一键部署

---
+++ docker/mysql/initdb.d/init.sql
@@ -0,0 +1,3 @@
+-- Initialize required databases for microservices
+CREATE DATABASE IF NOT EXISTS `linghuzhiyan_user` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `linghuzhiyan_auth` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
